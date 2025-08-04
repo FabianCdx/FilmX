0030144fcab65c90efdae0fff19a8fb9d0e7696e
Fabian Sander-Pirva <fabianpirva@gmail.com>
2025-08-04 03:59:26 +0200

    feature/minor-changes

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Spinner = () => {
   return (
     <div role="status" className="absolute-translate-x-1/2-translate-y-1/2 top-2/4 left-1/2">
--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ export interface SearchProps {
     searchTerm: string;
     setSearchTerm: (value: string) => void;
 }
+
 export interface Movie {
     id: number;
     title: string;
@@ -11,10 +12,3 @@ export interface Movie {
     release_date: string;
 }
 
-/* export interface MovieCardProps {
-    movie: Movie & {
-        vote_average: number;
-        original_language: string;
-        release_date: string;
-    }
-} */
